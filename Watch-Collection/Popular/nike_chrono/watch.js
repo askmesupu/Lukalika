f42c422680c5239ccb0e2069fa062f4771cb86b3
askmesupu <ask.me.pirated@gmail.com>
2025-11-29 01:22:53 +0600

    Create watch.js

---
+++ Watch-Collection/Popular/nike_chrono/watch.js
@@ -0,0 +1,12 @@
+{
+  "name": "Nike Chrono",
+  "brand": "Nike",
+  "price": 10000,
+  "discount": 15,
+  "stock": true,
+  "description": "Nike Chrono popular sports watch with silicone strap and chronograph.",
+  "images": [
+    "https://i.ibb.co/XXXXXX/nike-chrono1.jpg",
+    "https://i.ibb.co/XXXXXX/nike-chrono2.jpg"
+  ]
+}
